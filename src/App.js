fde61f93665a448edddac193321e98575794d66f
Jessicawatsonmiller <jessicawatsonmiller@gmail.com>
2020-06-01 16:33:31 -0700

    Yo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { ReactReader } from "react-reader";
+import { EpubView } from "react-reader";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save blah to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div style={{ position: "relative", height: "500px", width: "100%" }}>
+        {"Now Reading"}
+        {/*}* Container needs a height..*/}
+        <EpubView
+          url={"https://s3.amazonaws.com/epubjs/books/moby-dick/OPS/package.opf"}
+          title={"Moby Dick"}
+          location={"epubcfi(/6/2[cover]!/6)"}
+          locationChanged={epubcifi => console.log(epubcifi)}
+          tocChanged={toc => console.log(toc)}
+          zindex={"-10"}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
