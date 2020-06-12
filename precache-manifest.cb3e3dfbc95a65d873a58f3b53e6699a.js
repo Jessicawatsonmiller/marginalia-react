f1db578174a6c92a4feed70aa9979c35ff353f84
jessicawatsonmiller <jessicawatsonmiller@gmail.com>
2020-06-12 15:52:54 -0700

    Updates

---
+++ precache-manifest.cb3e3dfbc95a65d873a58f3b53e6699a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63076041ef6798a8e6f6e0f0dc69ee55",
+    "url": "/marginalia-react/index.html"
+  },
+  {
+    "revision": "8ea6c41a2c70071569c9",
+    "url": "/marginalia-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "244cd6d9e757c4c4e439",
+    "url": "/marginalia-react/static/js/2.e5d39690.chunk.js"
+  },
+  {
+    "revision": "6f36f33acbf70c47f4617e07fb08d92a",
+    "url": "/marginalia-react/static/js/2.e5d39690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ea6c41a2c70071569c9",
+    "url": "/marginalia-react/static/js/main.77794a9c.chunk.js"
+  },
+  {
+    "revision": "992c0ed9fa069d299996",
+    "url": "/marginalia-react/static/js/runtime-main.62259536.js"
+  }
+]);
